73453ef4140e94ca89b01b5c7e3514255f57795c
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-05-09 13:34:30 +0700

    integrate aws secret config as properties

--- src/main/java/com/jtrust/finetapi/FinetApiApplication.java
+++ src/main/java/com/jtrust/finetapi/FinetApiApplication.java
@@ -1,13 +1,33 @@
 package com.jtrust.finetapi;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
+@Slf4j
 public class FinetApiApplication {
+
+    @Value("#{aws.secret.manager}")
+    private Map<String, String> awsConfigProperties;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
 
     public static void main(String[] args) {
         SpringApplication.run(FinetApiApplication.class, args);
+
+
     }
 
+    @Bean
+    public void testBean(){
+        log.info("=========LOADING AWS SECRET MANAGER CONFIG=============");
+        log.info(gson.toJson(awsConfigProperties));
+    }
 }
