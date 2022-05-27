2f24876978525b98aa5cce854b0c5de1ca3a5f5e
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-05-27 09:05:31 +0700

    add local properties and update queueSqsService

--- src/main/java/com/jtrust/finetapi/service/QueueSqsService.java
+++ src/main/java/com/jtrust/finetapi/service/QueueSqsService.java
@@ -1,6 +1,8 @@
 package com.jtrust.finetapi.service;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.jtrust.finetapi.sqs.QueuePayload;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
@@ -13,12 +15,16 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Slf4j
 @Service
 @ConditionalOnProperty(name = "aws.sqs.enabled", havingValue = "true")
 public class QueueSqsService {
 
     private final QueueMessagingTemplate queueMessagingTemplate;
+
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Value("${aws.sqs.queueName}")
     private String queueName;
@@ -29,19 +35,21 @@ public class QueueSqsService {
     }
 
     /**
-     * @param message
+     * @param payload
      * {
      *     "source": "s3",
      *     "destination": "netApp",
      *     "uuid": "2342343243243sfaffasdfdsf"
      * }
      */
-    public void send(QueuePayload message) {
-        this.queueMessagingTemplate.send(queueName, MessageBuilder.withPayload(message).build());
+    public void send(QueuePayload payload) {
+        final String message = gson.toJson(payload);
+        log.info("sending message {} ", message);
+        this.queueMessagingTemplate.convertAndSend(queueName, payload);
     }
 
     @SqsListener(value = "${aws.sqs.queueName}", deletionPolicy = SqsMessageDeletionPolicy.ALWAYS)
-    public void receiveMessage(QueuePayload payload, @Header("header") String sendId) {
-        log.info("### Received message {}, with senderId {} ", payload, sendId);
+    public void receiveMessage(QueuePayload payload, @Header("headers") Map<String, String> headers) {
+        log.info("### Received message {}, with senderId {} ", gson.toJson(payload), gson.toJson(headers));
     }
 }
