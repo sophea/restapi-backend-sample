7e20f858ae59834235c0675b0b4422dbc873b2f9
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-06-02 16:41:05 +0700

    fix: test unit

--- src/main/java/com/jtrust/finetapi/FinetApiApplication.java
+++ src/main/java/com/jtrust/finetapi/FinetApiApplication.java
@@ -1,10 +1,11 @@
 package com.jtrust.finetapi;
 
+import io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+import io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-
+@SpringBootApplication(exclude = { ContextStackAutoConfiguration.class, ContextInstanceDataAutoConfiguration.class})
 public class FinetApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FinetApiApplication.class, args);
