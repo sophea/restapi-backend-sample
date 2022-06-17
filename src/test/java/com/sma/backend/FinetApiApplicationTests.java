28faa4ca89466ce1e68dfe7a2a1c5a86ed14e26f
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-06-17 09:31:15 +0700

    fix: test case remove unused package

--- src/test/java/com/sma/backend/FinetApiApplicationTests.java
+++ src/test/java/com/sma/backend/FinetApiApplicationTests.java
@@ -2,15 +2,14 @@ package com.sma.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FinetApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
