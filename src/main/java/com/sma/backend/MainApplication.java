c85dadf9d81e674e23101a015f72961999387918
Sophea Mak <sophea.mak@jtrustroyal.com>
2022-06-23 10:04:59 +0700

    feat: load startup application info host and server access

--- src/main/java/com/sma/backend/MainApplication.java
+++ src/main/java/com/sma/backend/MainApplication.java
@@ -2,8 +2,13 @@ package com.sma.backend;
 
 import io.awspring.cloud.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import io.awspring.cloud.autoconfigure.context.ContextStackAutoConfiguration;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * SpringBootApplication main application.
@@ -12,9 +17,45 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(
         exclude = {ContextStackAutoConfiguration.class, ContextInstanceDataAutoConfiguration.class})
+@Slf4j
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        final Environment env = SpringApplication.run(MainApplication.class, args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        final String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException ex) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info(
+                "\n----------------------------------------------------------\n\t"
+                        + "Application '{}' is running! Access URLs:\n\t"
+                        + "Local: \t\t{}://localhost:{}{}\n\t"
+                        + "External: \t{}://{}:{}{}\n\t"
+                        + "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
     }
 }
--- src/main/java/com/sma/backend/config/SqsConfig.java
+++ src/main/java/com/sma/backend/config/SqsConfig.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.messaging.config.QueueMessageHandlerFactory;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
+import java.util.Collections;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +16,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
-
-import java.util.Collections;
 
 /**
  * Sqs configuration.
